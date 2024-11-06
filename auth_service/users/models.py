b02f3e7f8cb336dfc9cbc1cd024403c58be6e163
YumoGOD <max.PROGO@gmail.com>
2024-11-06 09:13:47 +0300

    fixapi

--- auth_service/users/models.py
+++ auth_service/users/models.py
@@ -1,9 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class CustomUser(AbstractUser):
+    # Сохраняем уникальность поля email
     email = models.EmailField(unique=True)
 
+    # Указываем, что аутентификация будет использовать поле email, а не username
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    
+    # Если хотите, можете удалить поле username, так как оно уже не используется для аутентификации.
+    REQUIRED_FIELDS = []  # Пустой список, так как email будет использоваться для аутентификации.
+
+    def save(self, *args, **kwargs):
+        # Убедитесь, что username не пустое, если не используете его, установите пустое значение
+        if not self.username:
+            self.username = self.email  # Используем email в качестве username, если оно пустое
+        super().save(*args, **kwargs)
